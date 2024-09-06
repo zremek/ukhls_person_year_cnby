61807886c9c090d8ca9293844ba1cd42d4c8610c
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2024-09-06 14:03:37 +0200

    dates of next and previous interview done

--- 02-preparation.R
+++ 02-preparation.R
@@ -20,5 +20,29 @@ longfile <- longfile %>%
 
 
 # TODO FSTWAVE First wave R participated - survey year #### 
+# waiting for help https://iserredex.essex.ac.uk/support/issues/2144  
+
+
+
+# dates of next and previous interview #### 
+# Nxtwave_Yr Date of first interview after this year: year
+# Nxtwave_Mth Date of first interview after this year: month
+# Nxtwave_Day Date of first interview after this year: day
+
+# Prevwave_Yr Date of last interview after this year: year
+# Prevwave_Mth Date of last interview after this year: month
+# Prevwave_Day Date of last interview after this year: day
+
+longfile <- 
+  longfile %>% 
+  arrange(pidp, wave) %>% 
+  mutate(Prevwave_Yr = lag(intdaty_dv), 
+         Prevwave_Mth = lag(intdatm_dv), 
+         Prevwave_Day = lag(intdatd_dv), 
+         Nxtwave_Yr = lead(intdaty_dv), 
+         Nxtwave_Mth = lead(intdatm_dv), 
+         Nxtwave_Day = lead(intdatd_dv))
+
+
 
 
