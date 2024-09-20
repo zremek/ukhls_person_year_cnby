e6e5d7ea1dfaa7faa37e0e9f1043a221e4596cc0
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2024-09-20 11:22:29 +0200

    labels and minor fixes

--- 02-preparation.R
+++ 02-preparation.R
@@ -85,7 +85,7 @@ longfile <- longfile %>%
            JOB01_IncomeNet = if_else(!is.na(seearnnet_dv_na), 
                               true = seearnnet_dv_na, 
                               false = paynu_dv_na), 
-           Y_Workincome = fimnlabgrs_dv, 
+           Y_Workincome = fimnlabgrs_dv_na, 
            CY_Workincome = NA, 
            JOB01_IncomeGrs_CV = paygu_if == 1 | seearngrs_if == 1,
            JOB01_IncomeNet_CV = paynu_if == 1
@@ -158,17 +158,57 @@ longfile <- longfile %>%
 # RESIDSIZEa
 # Urban / rural
 
-# gor_dv+country
-# RESIDREGa
-# Region of residence
+longfile <- longfile %>%
+  mutate(
+    Respid = pidp, 
+    YRBIRTH = doby_dv,
+    MTBIRTH = NA,
+    DYBIRTH = NA,
+    GENDER = sex_dv,
+    RESIDSIZE = urban_dv,
+    RESIDREG = gor_dv,
+    MARITSTAT = marstat_dv,
+    CHILDREN = case_when(ndepchl_dv == -8 ~ 0,
+                         .default = ndepchl_dv)
+  ) %>%
+  labelled::drop_unused_value_labels()
 
-# marstat_dv+mastat_dv
-# MARITSTATa
-# Marital / partnership status
+# set variable labels #### 
 
-# nchild_dv+ndepchl_dv
-# CHILDRENa
-# Number of children
-
-
-
+ukhls_CNBformat <-
+  longfile %>%
+  select(starts_with(LETTERS, ignore.case = FALSE)) %>%
+  var_labels(
+    Respid = 'ID',
+    YEAR = 'Calendar year',
+    YRBIRTH = 'Year of birth',
+    MTBIRTH = 'Month of birth',
+    DYBIRTH = 'Day of birth',
+    GENDER = 'Gender',
+    RESIDSIZE = 'Urban / rural',
+    RESIDREG = 'Region of residence',
+    MARITSTAT = 'Marital / partnership status',
+    CHILDREN = 'Number of children',
+    FSTWAVE = 'First wave R participated - survey year',
+    LSTWAVE = 'Last wave R participated - survey year',
+    CURWAVE = 'First interview this year - survey year',
+    Curwave_Yr = 'Date of first interview this year: year',
+    Curwave_Mth = 'Date of first interview this year: month',
+    Curwave_Day = 'Date of first interview this year: day',
+    NXTWAVE = 'First interview after this year - survey year',
+    Nxtwave_Yr = 'Date of first interview after this year: year',
+    Nxtwave_Mth = 'Date of first interview after this year: month',
+    Nxtwave_Day = 'Date of first interview after this year: day',
+    PREVWAVE = 'Last interview before this year - survey year',
+    Prevwave_Yr = 'Date of last interview before this year: year',
+    Prevwave_Mth = 'Date of last interview before this year: month',
+    Prevwave_Day = 'Date of last interview before this year: day',
+    JOB01_ISCO08 = '[Job 01] ISCO-08 Code',
+    JOB01_IncomeGrs = '[Job 01] Monthly earnings, gross',
+    JOB01_IncomeNet = '[Job 01] Monthly earnings, net',
+    JOB01_IncomeGrs_CV = '[Job 01] Monthly earnings, gross: methodological information',
+    JOB01_IncomeNet_CV = '[Job 01] Monthly earnings, net: methodological information',
+    Y_Workincome = '[Income] Total income from work',
+    CY_Workincome = '[Derived income] Total income from work',
+    STUDY = 'Name of the panel study'
+  )
